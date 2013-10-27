d255c33461eb1ceb8e323847d24bff22d648a4b6
Justin Hall <jnhall@gmail.com>
2013-10-27 18:25:19 -0400

    Puts moonshot.nw in the correct directory

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
-				zip -r ../moonshot.nw source/*
+				zip -r moonshot.nw source/*
 				cat node-webkit.app/Contents/MacOS/node-webkit moonshot.nw > moonshot
 				chmod u+x moonshot        
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-				zip -r ../moonshot.nw source/*
+				zip -r moonshot.nw source/*
 				cat nw moonshot.nw > moonshot
 				chmod u+x moonshot        
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
