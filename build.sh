c71b4e6ba9c1e582b4378cab664897748da10641
Justin Hall <jnhall@gmail.com>
2013-10-27 18:15:41 -0400

    Adds node-webkit.app to gitignore (OS X), Adds OS X steps to README, create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+				zip -r ../moonshot.nw source/*
+				cat node-webkit.app/Contents/MacOS/node-webkit moonshot.nw > moonshot
+				chmod u+x moonshot        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+				zip -r ../moonshot.nw source/*
+				cat nw moonshot.nw > moonshot
+				chmod u+x moonshot        
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    cd source
+    pkzip -r ..\moonshot.nw *.*
+    cd ..
+				copy /b nw.exe+moonshot.nw moonshot.exe
+fi
+
